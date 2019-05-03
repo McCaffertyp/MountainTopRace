422401ec33e758003667b4606733befeeefce0ef
1MagicStar <49054703+1MagicStar@users.noreply.github.com>
2019-05-02 21:09:16 -0700

    Update GamePlay.java

--- GamePlay.java
+++ GamePlay.java
@@ -1,7 +1,19 @@
 import java.util.*;
 
-public class GamePlay {
-    public static void main(String[] args) {
-        System.out.println();
-    }
-}
+public class GamePlay 
+{
+    public static void main(String[] args) 
+    {
+        GamePlay start = new GamePlay();
+        start.run();
+    }//main
+    public static void run()
+    {
+           System.out.println("One fine day Buggs, Tweety, and Tazz were in a great race!");
+           System.out.println("Out of no where Marvin jumped into the race with his gun now everything has gone crazy!");
+           System.out.println("Who will make it to the top of the mount with the flag?");
+        
+           //This is where we start the multi-threading
+           //I am going to research multi-threading tonight
+    }//run
+}//class
